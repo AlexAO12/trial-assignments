8c1218cdc656938297e979689c9115ca696f706d
AlexAO12 <Alexander.Kalinin.g@gmail.com>
2020-08-31 01:29:02 +0700

    reload

--- #src/js/spoiler.js
+++ #src/js/spoiler.js
@@ -1,14 +1,19 @@
 const spoilerTitle = document.querySelector('.product-dscrp__spoiler-title');
 const buttonSpoiler = document.querySelector('.product-dscrp-body__button-spoiler');
 const socialBlock = document.querySelector('.product-dscrp__spoiler-social');
+const socialBlockWrapp = document.querySelector('.product-dscrp__spoiler-body');
 
 spoilerTitle.addEventListener('click', function (e) {
     e.preventDefault();
-    spoilerTitle.classList.toggle('product-dscrp__spoiler-title--arrow');
-    spoilerTitle.nextElementSibling.classList.toggle('product-dscrp__spoiler-body--visible');
+    setTimeout(function () {
+        spoilerTitle.classList.toggle('product-dscrp__spoiler-title--arrow');
+        socialBlockWrapp.classList.toggle('product-dscrp__spoiler-body--visible');
+    }, 300);
 });
 
 buttonSpoiler.addEventListener('click', function (e) {
     e.preventDefault();
-    socialBlock.classList.toggle('product-dscrp__spoiler-social--visible');
+    setTimeout(function () {
+        socialBlock.classList.toggle('product-dscrp__spoiler-social--visible');
+    }, 500);
 });
